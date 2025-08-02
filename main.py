19bcbc8679392866e0393f468cb3a7bcc656d352
Skamakka <zscamacca@outlook.com>
2025-08-02 15:33:35 -0400

    add pygame sprite groups for better object management

--- main.py
+++ main.py
@@ -2,11 +2,19 @@ import pygame
 from constants import *
 from player import Player
 
+
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     dt = 0
 
     while True:
@@ -14,10 +22,13 @@ def main():
             if event.type == pygame.QUIT:
                 return
             
-        player.update(dt)
+        updatable.update(dt)
 
         screen.fill((0,0,0))
-        player.draw(screen)
+
+        for sprite in drawable:
+            sprite.draw(screen)
+
         pygame.display.flip()
 
         dt = clock.tick(60) / 1000
